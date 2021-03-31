b0fac74b874e1fa5492359cda00fd3f4ffd4e501
JPSiler <jsixx77@hotmail.com>
2021-03-31 09:14:16 -0500

    initial connection to graphQL complete

--- server.js
+++ server.js
@@ -2,8 +2,26 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const app = express()
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql')
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: { type: GraphQLString,
+            resolve: () => "Hello World"
+            }
+        })
+    })
+})
 
 
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
 app.listen(5000, () => console.log('Listening on port 5000'
 ))
